f8de9e2afdf207caf5932bb5bd626e8077a6f973
bhanu Mannepalli <bhanumannepalli7@gmail.com>
2025-12-05 09:43:03 +0530

    Add files via upload

---
+++ 2d array.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int a[3][3],i,j;
+	printf(" enter the array elements:");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	printf("array elements are");
+	for(i=0;i>3;i++)
+	{
+		for(j=0;j<3;j++)
+		printf("%d",a[i][j]);
+	}
+	printf("\n");
+	return 0;
+}
